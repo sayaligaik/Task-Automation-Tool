34ba9749a1cf67a2a58123f9957286501f5c5bb0
Sayali Gaikwad <124872737+sayaligaik@users.noreply.github.com>
2024-09-27 21:54:16 +0530

    TaskController.java

---
+++ TaskController.java
@@ -0,0 +1,23 @@
+package com.taskautomation;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task addTask(@RequestBody Task task) {
+        return taskService.addTask(task);
+    }
+}
